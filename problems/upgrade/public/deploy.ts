d0d93650c581fb62778c2677b029a7a8416df173
Yushi Sato <yushi.kinger@gmail.com>
2021-07-27 23:10:38 +0900

    setup done

--- problems/upgrade/public/deploy.ts
+++ problems/upgrade/public/deploy.ts
@@ -58,23 +58,23 @@ export const deploy = async (deployer: Sender): Promise<string> => {
     console.log('public/Setup.sol', contractAddress);
     console.log('');
 
-    // /* change admin */
-    // {
-    //     const owner = "0x807a96288A1A408dBC13DE2b1d087d10356395d2";
-    //     const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    //     const data = Buffer.concat([Buffer.from("8f283970000000000000000000000000"), Buffer.from(contractAddress.slice(2))]);
-    //     const receipt = await deployer.callContractFromUnknown(owner, usdc, data);
-    //     console.log('call changeAdmin():', receipt);
-    //     console.log('');
-    // }
-
-    // /* call Setup.upgrade() method */
-    // {
-    //     const data = abi.simpleEncode('upgrade()');
-    //     const receipt = await deployer.callContract(contractAddress, data);
-    //     console.log('call upgrade():', receipt);
-    //     console.log('');
-    // }
+    /* change admin */
+    {
+        const owner = "0x807a96288A1A408dBC13DE2b1d087d10356395d2";
+        const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+        const data = Buffer.concat([Buffer.from('8f283970000000000000000000000000', 'hex'), Buffer.from(contractAddress.slice(2), 'hex')]);
+        const receipt = await deployer.callContractFromUnknown(owner, usdc, data);
+        console.log('call changeAdmin():', receipt);
+        console.log('');
+    }
+    
+    /* call Setup.upgrade() method */
+    {
+        const data = abi.simpleEncode('upgrade()');
+        const receipt = await deployer.callContract(contractAddress, data);
+        console.log('call upgrade():', receipt);
+        console.log('');
+    }
 
     return contractAddress;
 }
